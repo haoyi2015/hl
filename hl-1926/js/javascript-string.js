c4ff6fd54bb812eca529d4d1b99a92598996f6cf
Apple <heshixi@peilian.com>
2019-11-07 11:33:47 +0800

    常用javascript组装更新

---
+++ hl-1926/js/javascript-string.js
@@ -0,0 +1,98 @@
+/**
+* JavaScript日常高频使用的工具方法组装
+*/
+
+ // 4. String 字符串操作
+
+/**
+ * 去除空格
+ * @param  {str}
+ * @param  {type}
+ *       type:  1-所有空格  2-前后空格  3-前空格 4-后空格
+ * @return {String}
+ */
+trim (str, type) {
+    type = type || 1
+    switch (type) {
+        case 1:
+            return str.replace(/\s+/g, "");
+        case 2:
+            return str.replace(/(^\s*)|(\s*$)/g, "");
+        case 3:
+            return str.replace(/(^\s*)/g, "");
+        case 4:
+            return str.replace(/(\s*$)/g, "");
+        default:
+            return str;
+    }
+}
+
+/**
+ * @param  {str}
+ * @param  {type}
+ *       type:  1:首字母大写  2：首页母小写  3：大小写转换  4：全部大写  5：全部小写
+ * @return {String}
+ *  Js中文网 - 前端进阶资源教程 [www.javascriptC.com](https://www.javascriptc.com) ，javascirpt官方唯一指定中文网
+ * 一个帮助开发者成长的社区，**你想要的，在这里都能找到**
+ */
+changeCase (str, type) {
+    type = type || 4
+    switch (type) {
+        case 1:
+            return str.replace(/\b\w+\b/g, function (word) {
+                return word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
+
+            });
+        case 2:
+            return str.replace(/\b\w+\b/g, function (word) {
+                return word.substring(0, 1).toLowerCase() + word.substring(1).toUpperCase();
+            });
+        case 3:
+            return str.split('').map( function(word){
+                if (/[a-z]/.test(word)) {
+                    return word.toUpperCase();
+                }else{
+                    return word.toLowerCase()
+                }
+            }).join('')
+        case 4:
+            return str.toUpperCase();
+        case 5:
+            return str.toLowerCase();
+        default:
+            return str;
+    }
+}
+
+/*
+    检测密码强度
+*/
+checkPwd (str) {
+    var Lv = 0;
+    if (str.length < 6) {
+        return Lv
+    }
+    if (/[0-9]/.test(str)) {
+        Lv++
+    }
+    if (/[a-z]/.test(str)) {
+        Lv++
+    }
+    if (/[A-Z]/.test(str)) {
+        Lv++
+    }
+    if (/[\.|-|_]/.test(str)) {
+        Lv++
+    }
+    return Lv;
+}
+
+/*过滤html代码(把<>转换)*/
+filterTag (str) {
+    str = str.replace(/&/ig, "&");
+    str = str.replace(/</ig, "<");
+    str = str.replace(/>/ig, ">");
+    str = str.replace(" ", " ");
+    return str;
+}
+
